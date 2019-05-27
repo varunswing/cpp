f2c25cb37d7d91dd07e3e918580601f722fd1f49
varunswing <varuns939@gmail.com>
2019-05-27 19:19:34 +0530

    Create comment.cpp

---
+++ noob/comment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /*
+    This is used to comment a whole paragraph.
+    */
+
+   cout << "There are two types of comment."; //This is used to comment only a single line.
+}
