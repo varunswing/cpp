7caabeb2da134180458dfc10a00c79869ce09c4f
varunswing <varuns939@gmail.com>
2019-05-27 19:12:17 +0530

    Create cin.cpp

---
+++ noob/cin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    float average;
+
+    cout << "Enter your age and average." <<endl;
+
+    cin >> age >> average;
+
+    cout << "Age is " << age << " and average is " << average;
+
+    return 0;
+}
