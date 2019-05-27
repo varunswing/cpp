4d8661ca026bf76743a83318b0b24baee50bb9bf
varunswing <varuns939@gmail.com>
2019-05-27 19:19:47 +0530

    Create variables.cpp

---
+++ noob/variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b=10,c;
+    int num = 23;
+    float phone;
+    char name = 'j';
+    bool isit = true;
+
+    phone = 764.54;
+
+    cout << b << endl;
+
+    cout << num << endl << phone << endl << name << endl << isit;
+
+    return 0;
+}
