34deda084e89c4ec5a10fe404bb07d54d9a943ee
varunswing <varuns939@gmail.com>
2019-05-27 19:12:13 +0530

    Create arithmeticOperator.cpp

---
+++ noob/arithmeticOperator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 100, b=20;
+
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b << endl;
+}
