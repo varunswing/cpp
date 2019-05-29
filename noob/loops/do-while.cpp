b5b3d4de59967f6e5e6791560c20f75275dc3c8e
varunswing <varuns939@gmail.com>
2019-05-29 10:18:49 +0530

    Create do-while.cpp

---
+++ noob/loops/do-while.cpp
@@ -0,0 +1,13 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+    int input;
+    cin >> input;
+
+    do {
+        cout << input << endl;
+        input--;
+    }
+    while(input != 0);
+}
