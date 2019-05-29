acb4f5bc8e35103db90f18bd68dbc20948e2aa50
varunswing <varuns939@gmail.com>
2019-05-29 10:18:41 +0530

    Create function.cpp

---
+++ noob/function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void display(); //Function prototype
+
+int main(){
+    display();
+    display();
+    return 0;
+}
+
+void display(){
+    cout << "Hello" << endl;
+}
