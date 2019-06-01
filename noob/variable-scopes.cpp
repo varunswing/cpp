164de6e1b31aa6472a31e392f5efd45240c1924c
varunswing <varuns939@gmail.com>
2019-06-01 07:42:31 +0530

    Create variable-scopes.cpp

---
+++ noob/variable-scopes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void display();
+
+int a;
+
+int main(){
+
+    display();
+
+    return 0;
+}
+
+void display(){ 
+    int x;
+    cout << x << endl;
+    cout << a;
+}
