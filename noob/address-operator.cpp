15f5bf00eaee1085befa9cc4d480c05ad4549477
varunswing <varuns939@gmail.com>
2019-06-01 15:17:53 +0530

    Create address-operator.cpp

---
+++ noob/address-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age = 100, weight = 49;
+
+    cout << age << endl;
+    cout << &age << endl;
+
+    cout << weight << endl;  
+    cout << &weight << endl;
+
+    return 0;
+}
