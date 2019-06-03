2c14beee2d516b0a5bfa9f881761c82077352612
varunswing <varuns939@gmail.com>
2019-06-04 01:04:34 +0530

    Create oops.cpp

---
+++ noob/oops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class human{
+public:
+    void display(){
+        cout << "Hey! I am Varun.";
+    }
+};
+
+
+int main(){
+    human obj;
+    obj.display();
+    return 0;
+}
