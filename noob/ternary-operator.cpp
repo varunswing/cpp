31fbcca7ed4617de91165aad5483ae8bdbb3cca0
varunswing <varuns939@gmail.com>
2019-06-05 22:45:03 +0530

    Create ternary-operator.cpp

---
+++ noob/ternary-operator.cpp
@@ -0,0 +1,11 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+    int marks;
+    cout << "Enter your marks ->" << endl;
+    cin >> marks;
+    marks >= 35 ? cout << "You are passed." : cout << "You are failed.";
+
+   return 0;
+}
