6d859d933d8a6bcdd214301ecdb50ca6f8d99f06
varunswing <varuns939@gmail.com>
2019-06-13 21:55:06 +0530

    Create manipulators.cpp

---
+++ manipulators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    cout << "Hi" << endl;
+    cout << hex << 100 << endl;
+
+    cout << setw(10) << setfill('@') << "varun" << endl;
+
+    cout << "Nothing" << endl;
+
+    return 0;
+}
