8e6aeac61cd7df16fa3b85b2c159c01610deb7b3
varunswing <varuns939@gmail.com>
2019-06-15 21:04:53 +0530

    Update binary-file.cpp

--- binary-file.cpp
+++ binary-file.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
+
 
 using namespace std;
 
@@ -7,15 +9,19 @@ int main(){
     char input[100];
     strcpy(input, "Learning lad rocks.");
 
-    fstream file("varun.txt", ios::out | ios::in | ios::binary | ios::trunc);
+    fstream file("varun.bin", ios::out | ios::in | ios::binary | ios::trunc);
     if(!file.is_open()){
         cout << "File not opening." << endl;
     }else{               
         int length = strlen(input);
-        for(i=0; i<=length; i++){
+        for(int i=0; i<=length; i++){
             file.put(input[i]);
         }
         file.seekg(0);
         char ch;
-
+        while(file.good()){
+            file.get(ch);
+            cout << ch;
+        }
     }
+}
