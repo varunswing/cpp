96cbc6b7618cf37b343c63f3a5f33dbca2b78173
varunswing <varuns939@gmail.com>
2019-06-17 22:05:47 +0530

    Create person_class.cpp

---
+++ person_class.cpp
@@ -0,0 +1,11 @@
+class Person{
+    string name;
+    int age;
+    public:
+    Person(string name, int age){
+        cout << "Hi my name is " << name << ".\nI am " << age << " years old.";
+    } 
+    void display(){
+
+    }
+};
