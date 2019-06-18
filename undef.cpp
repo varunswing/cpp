1bd477e15550e0eb187337aa23c7d63387394b1f
varunswing <varuns939@gmail.com>
2019-06-18 20:34:30 +0530

    Create undef.cpp

---
+++ undef.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define Windows 1
+
+using namespace std;
+
+int main(){
+    #ifdef Windows
+        cout << "Windows user" << endl;
+    #endif
+
+    #undef Windows
+    
+    #ifndef Windows
+        cout << "Linux user";
+    #else
+        cout << "Mac user";
+    #endif   
+
+    return 0;
+}
