bcc2fe109e5cc3d8a8be51ddca4925606e3243bb
varunswing <varuns939@gmail.com>
2019-06-20 09:17:24 +0530

    Create Person.cpp

---
+++ noob/writing-classes-in-separate-file.cpp/sources/Person.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Person.h"
+
+using namespace std;
+
+Person::Person(){
+    cout << "Person class constructer." << endl;
+}
+
+void Person::whatYouGot(){
+    cout << "I got billion." << endl;
+}
