746274e54aa28899474edc4438ffd7c73dcdcd5b
varunswing <varuns939@gmail.com>
2019-08-21 22:18:04 +0530

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+    cout << i;
+    }
+
+    return 0;
+}
